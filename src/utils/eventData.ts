af4ead645018b91d0166a92b03146ec5f48d4b12
S-KALAIKUMAR <162897601+S-KALAIKUMAR@users.noreply.github.com>
2025-03-24 20:27:54 +0530

    Update eventData.ts

--- src/utils/eventData.ts
+++ src/utils/eventData.ts
@@ -21,7 +21,7 @@ export const eventData: EventType[] = [
     description: 'Trace Back Coding is a reverse coding challenge where you don’t get the problem statement—just the output and conditions! Your task is to analyze the given constraints, think logically, and write the correct code to match the expected output. If you love problem-solving and coding challenges, this is the ultimate test of your skills!',
     image:  'images/trace.jpg', 
     date: '2025-03-29',
-    time: '10:00 AM - 1:00 PM',
+    time: '10:00 AM - 12:30 PM',
     type: 'technical',
     rules: [
       "Open to all individual participants interested in solving coding challenges with logic and skill." ,  
@@ -52,7 +52,7 @@ export const eventData: EventType[] = [
     description: 'Prompt Chef is a technical event where participants craft precise AI image generation prompts based on given themes. Compete individually to create the most effective prompts within a fixed time. Showcase your creativity and AI skills in this unique challenge!',
     image: 'images/super.webp',
     date: '2025-03-29',
-    time: '10:00 AM - 1:00 PM',
+    time: '10:00 AM - 12:30 PM',
     type: 'technical',
     rules: [
       "Open to all individual participants interested in AI-generated images and prompt creation challenges." ,
@@ -78,7 +78,7 @@ export const eventData: EventType[] = [
     description: 'INNOV8 is a technical event  where you can bring your ideas to life! Whether you have a groundbreaking concept or a prototype project, this is your chance to showcase it. Present your ideas using a PPT or a working prototype and impress the judges with your innovation, problem-solving skills, and creativity. Let your ideas shape the future!',
     image: 'images/innov8.jpg',
     date: '2025-03-29',
-    time: '10:00 AM - 1:00 PM',
+    time: '10:00 AM - 12:30 PM',
     type: 'technical',
     rules: [
       "Open to all individuals and teams with a maximum size of four members for participation." ,
@@ -108,10 +108,10 @@ export const eventData: EventType[] = [
   {
     id: 'meme-media',
     title: 'Reinz Auction',
-    description: "Rinz Auction is an exciting non-technical event inspired by the IPL auction! Participants will step into the shoes of team owners, strategizing and bidding to build the best possible team within a fixed budget. Make smart decisions, outbid your rivals, and form the ultimate squad. It's all about strategy, planning, and quick thinking—are you ready to take the challenge?",
+    description: "Reinz Auction is an exciting non-technical event inspired by the IPL auction! Participants will step into the shoes of team owners, strategizing and bidding to build the best possible team within a fixed budget. Make smart decisions, outbid your rivals, and form the ultimate squad. It's all about strategy, planning, and quick thinking—are you ready to take the challenge?",
     image: 'images/Reinz auction.jpg',
     date: '2025-03-29',
-    time: '2:00 PM - 4:00 PM',
+    time: '11:00 AM - 2:00 PM',
     type: 'non-technical',
     rules: [
       "A list of players for bidding will be provided, categorized into groups with base prices." ,
@@ -145,7 +145,7 @@ export const eventData: EventType[] = [
     description: "Time Rush is a fun team-based event where participants must complete a series of exciting and time-limited challenges. The tasks are designed to test your team's speed, coordination—all while having loads of fun! Each round brings a new surprise, and the difficulty increases as you progress. If your team fails to complete a task within the time limit, you're disqualified! Can your team survive the rush and emerge as the ultimate winners?",
     image: 'images/time.jpg',
     date: '2025-03-29',
-    time: '2:00 PM - 4:00 PM',
+    time: '1:30 PM - 3:00 PM',
     type: 'non-technical',
     rules: [
       "The event is team-based, allowing a maximum of four members per team to participate." ,
@@ -176,7 +176,7 @@ export const eventData: EventType[] = [
     description: "Join our hands-on workshop to learn how to build AI-powered applications from scratch! This session is designed for beginners and enthusiasts who want to explore the potential of AI in real-world applications. Through step-by-step guidance, you will learn the fundamentals of AI development and build your own AI-based tool. No prior AI experience is required—just curiosity and a willingness to learn!",
     image: 'images/workshop.webp',
     date: '2025-03-29',
-    time: '10:00 AM - 1:00 PM',
+    time: '10:30 AM - 1:30 PM',
     type: 'workshop',
     rules: [
       "Open to all students, and no prior AI knowledge is required to participate in the session." ,
